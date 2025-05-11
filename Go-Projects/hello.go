256cfba4a3524b6e1bcb60eed438ff3352ceafbe
Rafreaks06 <mr.rafreaks@gmail.com>
2025-05-11 22:34:21 +0700

    menambahkan file test

---
+++ Go-Projects/hello.go
@@ -0,0 +1,11 @@
+package main // Setiap file Go harus punya package declaration
+
+import "fmt" // Import package untuk input/output
+
+func main() {
+	// Menampilkan pesan ke console
+	fmt.Println("Hello, World!")
+
+	// Bisa juga menggunakan Printf untuk formatting
+	fmt.Printf("Halo, ini program Go pertamaku!\n")
+}
